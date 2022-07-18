6c6af75aa0daca1ce73ee1b9e9f1f88782f5d5a4
hugoroniaG <102172552+hugoroniaG@users.noreply.github.com>
2022-07-18 17:58:58 -0500

    added validations to our reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,14 +16,20 @@ class ReviewsController < ApplicationController
 	end
 	
 	def create
-		# take info from the form and add it to the database
+		# take info from the form and add it to the model
 		@review = Review.new(form_params)
 		
-		#save this  to the database
-		@review.save
+		# we want to check if the model can be saved
+		# if it is, we go to the home page again
+		# if it isn't, show the new form
+		if @review.save
+			redirect_to root_path
+		else
+			# show the view for new.html.erb
+			render "new", status: :unprocessable_entity
+		end
 		
-		#redirect back to the homepage
-		redirect_to root_path
+		
 	end
 	
 	def show
@@ -54,9 +60,12 @@ class ReviewsController < ApplicationController
 		# find the individual review
 		@review = Review.find(params[:id])
 		# update with the new info from the form
-		@review.update(form_params)
+		if @review.update(form_params)
 		# redirect somewhere new
-		redirect_to review_path(@review)
+			redirect_to review_path(@review)
+		else
+			render "edit", status: :unprocessable_entity
+		end
 	end
 	
 	def form_params
