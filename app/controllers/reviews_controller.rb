7e4adb9d93136f47b654e034f3eecec49c4e0a97
hugoroniaG <102172552+hugoroniaG@users.noreply.github.com>
2022-07-19 13:53:48 -0500

    added filters

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,22 @@
 class ReviewsController < ApplicationController
 	
 	def index 
-	# this is our list page for reviews	
-	
-		@number = rand(100)
+		# this is our list page for reviews	
+		@price = params[:price]
+		@cuisine = params[:cuisine]
 		
+		# start with all the reviews
 		@reviews = Review.all
 		
+		# filtering by price
+		if @price.present?
+			@reviews = @reviews.where(price: @price)
+		end
+		
+		# filter by cuisine
+		if @cuisine.present?
+			@reviews = @reviews.where(cuisine: @cuisine)
+		end
 	end
 	
 	def new
@@ -69,7 +79,7 @@ class ReviewsController < ApplicationController
 	end
 	
 	def form_params
-		params.require(:review).permit(:title, :restaurant, :body, :score, :ambiance)
+		params.require(:review).permit(:title, :restaurant, :body, :score, :ambiance, :cuisine, :price)
 	end
 	
 		
